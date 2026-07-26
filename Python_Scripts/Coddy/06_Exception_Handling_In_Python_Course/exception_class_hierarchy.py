ccbbd43e0c92d536c5cbd9a6a267b58b94ec1b93
Muhammad El-Adeeb <muhammadaligg33ee@gmail.com>
2026-07-26 07:57:48 +0300

    Implement observe function for exception handling demo
    Added a function to demonstrate exception handling and class hierarchy.

---
+++ Python_Scripts/Coddy/06_Exception_Handling_In_Python_Course/exception_class_hierarchy.py
@@ -0,0 +1,8 @@
+#Click on RUN CODE and observe
+def observe(idx):
+    try:
+        data = [10,20,30,40]
+        print(data[idx])  #IndexError
+    except Exception as e:
+        print(type(e)) #Shows Exception is class
+        print(IndexError.mro()) #Shows all parent classes
