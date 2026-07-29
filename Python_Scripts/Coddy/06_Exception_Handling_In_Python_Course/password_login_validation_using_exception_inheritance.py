1ade685a438dca885970a42dfd27573fc93a17e1
Muhammad El-Adeeb <muhammadaligg33ee@gmail.com>
2026-07-29 07:56:59 +0300

    Implement custom exception for password validation
    Refactor password validation to use custom exception handling.

---
+++ Python_Scripts/Coddy/06_Exception_Handling_In_Python_Course/password_login_validation_using_exception_inheritance.py
@@ -0,0 +1,30 @@
+stored_password = "12345" #fetched from database (Imagine)
+class Invalid_Password(Exception):
+    pass
+def login():
+    try:
+        password = input()
+        if password!=stored_password:
+            raise Invalid_Password("Invalid password, try again")
+    except Exception as e:
+        print(e)
+        login()
+    else:
+        print("session created")
+# Another Solution:
+"""
+stored_password = "12345" #fetched from database (Imagine)
+class Invalid_Password(Exception):
+    pass
+def login():
+    while True:
+        try:
+            password = input()
+            if password != stored_password:
+                raise Invalid_Password("Invalid password, try again")
+        except Exception as obj:
+            print(obj)
+        else:
+            print("session created")
+            break
+"""
