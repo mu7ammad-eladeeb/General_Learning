70a1f3a54dfc4a95382656f8530cf146ce38c4f1
Muhammad El-Adeeb <muhammadaligg33ee@gmail.com>
2026-07-30 06:56:23 +0300

    Implement custom exceptions for bank transactions

---
+++ Python_Scripts/Coddy/06_Exception_Handling_In_Python_Course/bank_transaction_custom_exceptions.py
@@ -0,0 +1,40 @@
+balance = 10000  # Imagine it is fetched from database.
+
+class BalanceException(Exception):
+    pass
+
+class AttemptException(Exception):
+    pass
+
+attempt = 1
+
+def transaction():
+    global balance, attempt
+
+    try:
+        amt = float(input())
+
+        # Check remaining amount after transaction
+        temp = balance - amt
+
+        if temp < 100:
+            raise BalanceException("Insufficient balance")
+
+    except Exception as obj:
+        print(obj)
+
+        try:
+            if attempt == 3:
+                raise AttemptException("No more attempts allowed!")
+
+            attempt += 1
+            transaction()
+
+        except Exception as e:
+            print(e)
+
+    else:
+        balance = balance - amt
+        print("Transaction is success, Remaining balance is:", balance)
+
+transaction()
