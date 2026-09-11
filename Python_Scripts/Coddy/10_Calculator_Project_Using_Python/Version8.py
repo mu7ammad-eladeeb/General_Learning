9dfdd79bb75912dbe4a82bff7db3120705690735
Muhammad El-Adeeb <233196324+mu7ammad-eladeeb@users.noreply.github.com>
2026-09-11 14:58:57 +0300

    Added The Moved Folder After It Is Renamed

---
+++ Python_Scripts/Coddy/10_Calculator_Project_Using_Python/Version8.py
@@ -0,0 +1,59 @@
+def calc(operator, num1, num2=None):
+    operator = operator.lower()
+    
+    if not isinstance(num1, (int, float)):
+        raise Exception(f'Invalid number "{num1}"')
+
+    if num2 is not None and not isinstance(num2, (int, float)):
+        raise Exception(f'Invalid number "{num2}"')
+
+    if operator not in ['+', '-', '*', '/', '^', '%',
+                        'add', 'sub', 'mul', 'div', 'pow', 'mod']:
+        raise Exception(f'Invalid operator "{operator}"')
+
+    # Single number operations
+    if num2 is None:
+        if operator == '+' or operator == 'add':
+            return num1
+        elif operator == '-' or operator == 'sub':
+            return -num1
+        else:
+            raise Exception(f'Invalid operator "{operator}"')
+
+    # Two number operations
+    if operator == '+' or operator == 'add':
+        return num1 + num2
+    elif operator == '-' or operator == 'sub':
+        return num1 - num2
+    elif operator == '*' or operator == 'mul':
+        return num1 * num2
+    elif operator == '/' or operator == 'div':
+        if num2 == 0:
+            raise Exception("Division by zero")
+        return num1 / num2
+    elif operator == '^' or operator == 'pow':
+        return num1 ** num2
+    elif operator == '%' or operator == 'mod':
+        if num2 == 0:
+            raise Exception("Division by zero")
+        return num1 % num2
+# eval function to unpack a list
+def eval(lst):
+    if len(lst) == 2:
+        operator, num1 = lst
+
+        if isinstance(num1, list):
+            num1 = eval(num1)
+
+        return calc(operator, num1)
+
+    elif len(lst) == 3:
+        operator, num1, num2 = lst
+
+        if isinstance(num1, list):
+            num1 = eval(num1)
+
+        if isinstance(num2, list):
+            num2 = eval(num2)
+
+        return calc(operator, num1, num2)
